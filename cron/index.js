e6cf504ac6deb51b06493a8870646b1f8c8c5e6f
Gaurang Mangukiya <36733487+gaurangmangukiya@users.noreply.github.com>
2024-11-24 20:06:22 +0530

    :sparkle: feat: Every Saturday update

--- cron/index.js
+++ cron/index.js
@@ -4,7 +4,7 @@ const {processEmails} = require("./mailSend");
 
 function initCron() {
     cron.schedule('30 7 * * *', processEmails);
-    cron.schedule('30 18 * * *', processDomains);
+    cron.schedule('0 0 * * SAT', processDomains);
 }
 
 module.exports = {initCron};
