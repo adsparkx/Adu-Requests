345f0eba510ecfb425b283f3d9bab8722ce391f2
Gaurang Mangukiya <36733487+gaurangmangukiya@users.noreply.github.com>
2024-11-24 19:55:13 +0530

    :sparkle: feat: Mail Send Process updated

--- cron/index.js
+++ cron/index.js
@@ -4,7 +4,7 @@ const {processEmails} = require("./mailSend");
 
 function initCron() {
     cron.schedule('30 7 * * *', processEmails);
-    cron.schedule('18 30 * *', processDomains);
+    cron.schedule('18 30 * * *', processDomains);
 }
 
 module.exports = {initCron};
